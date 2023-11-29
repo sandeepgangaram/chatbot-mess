32745e77b21b138decf2f4bfa027faaff3652492
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-11-29 08:03:19 +0530

    move keys to env

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: '',
-    GOOGLE_PROJECT_ID: '',
-    DF_LANGUAGE_CODE: '',
-    GOOGLE_CLIENT_EMAIL: '',
-    GOOGLE_PRIVATE_KEY: ''
+  SERVER_URL: "https://chatbot-v1-z51q.onrender.com/",
+  GOOGLE_PROJECT_ID: "",
+  DF_LANGUAGE_CODE: "",
+  GOOGLE_CLIENT_EMAIL: "",
 };
